3b0aac5f8e6f29699889dcc69bb8c03224d27b7d
Omari Mayers-Walker <omari@fastmail.fm>
2015-09-15 16:10:32 -0400

    added delete task and check complete

--- shared-todo.js
+++ shared-todo.js
@@ -3,7 +3,7 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
     Template.body.helpers({
         tasks: function(){
-            return Tasks.find({});
+            return Tasks.find({}, {sort: {createdAt: -1}});
         }
     });
     Template.body.events({
@@ -25,6 +25,17 @@ if (Meteor.isClient) {
             // clear form
             event.target.text.value = "";
             event.target.number.value = "";
+        }
+    });
+
+    Template.task.events({
+        "click .toggle-checked": function(){
+            Tasks.update(this._id, {
+                $set: {checked: ! this.checked}
+            });
+        },
+        "click .delete": function(){
+            Tasks.remove(this._id);
         }
     });
 }
