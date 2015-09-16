97a658f4f3bd12c95c669a3e18cb4a7237ee0894
Omari Mayers-Walker <omari@fastmail.fm>
2015-09-15 21:24:56 -0400

    added incomplete counter

--- shared-todo.js
+++ shared-todo.js
@@ -13,8 +13,10 @@ if (Meteor.isClient) {
         },
         hideCompleted: function(){
             return Session.get("hideCompleted");
+        },
+        incompleteCount: function(){
+            return Tasks.find({checked: {$ne: true}}).count();
         }
-
     });
     Template.body.events({
         "submit .new-task": function(event){
