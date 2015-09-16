3e24cf4bf147858bba1da3c2bbf12f31ef160e88
Omari Mayers-Walker <omari@fastmail.fm>
2015-09-15 21:08:27 -0400

    added hide completed

--- shared-todo.js
+++ shared-todo.js
@@ -3,8 +3,18 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
     Template.body.helpers({
         tasks: function(){
-            return Tasks.find({}, {sort: {createdAt: -1}});
+            // return Tasks.find({}, {sort: {createdAt: -1}});
+            if (Session.get("hideCompleted")){
+                // if hide completed, filter tasks
+                return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+            } else {
+                return Tasks.find({}, {sort: {createdAt: -1}});
+            }
+        },
+        hideCompleted: function(){
+            return Session.get("hideCompleted");
         }
+
     });
     Template.body.events({
         "submit .new-task": function(event){
@@ -25,6 +35,9 @@ if (Meteor.isClient) {
             // clear form
             event.target.text.value = "";
             event.target.number.value = "";
+        },
+        "change .hide-completed input": function(event){
+            Session.set("hideCompleted", event.target.checked);
         }
     });
 
